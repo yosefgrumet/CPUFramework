9a4aa23d94be580861c4e84897865516d35d9f23
yosefgrumet <yosefgrumet@gmail.com>
2024-05-14 22:50:17 -0400

    Added CRUD

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -20,9 +20,23 @@ namespace CPUFramework
             cmd.CommandText = sqlstatement;
             var dr = cmd.ExecuteReader();
             dt.Load(dr);
+
+            SetAllColumnsAllowNull(dt);
+
             return dt;
         }
 
+        public static void ExecuteSQL(string sqlstatement)
+        {
+            GetDataTable(sqlstatement);
+        }
+        private static void SetAllColumnsAllowNull (DataTable dt)
+        {
+            foreach (DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            }
+        }
         public static void DebugPrintDataTable(DataTable dt)
         {
             foreach(DataRow r in dt.Rows)
