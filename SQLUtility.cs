0c0d451a0c550de486df11cdfc7475ba5dd6cb78
yosefgrumet <yosefgrumet@gmail.com>
2024-06-21 11:37:12 -0400

    #44 hw16

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -30,6 +30,22 @@ namespace CPUFramework
         {
             GetDataTable(sqlstatement);
         }
+
+        public static int GetFirstColumnFirstRowValue(string sql)
+        {
+            int n = 0;
+
+            DataTable dt = GetDataTable(sql);
+            if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            {
+                if (dt.Rows[0][0] != DBNull.Value)
+                {
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
+                }
+            }
+
+            return n;
+        }
         private static void SetAllColumnsAllowNull (DataTable dt)
         {
             foreach (DataColumn c in dt.Columns)
